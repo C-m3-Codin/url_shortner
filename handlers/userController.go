59767a638149ed73556036e67d9fef03f3226c42
c1337 <cyril199897@gmail.com>
2023-05-09 00:32:14 +0530

    user controller

---
+++ handlers/userController.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/c-m3-codin/url_shortner/models"
+	"github.com/c-m3-codin/url_shortner/services"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterUser(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+
+	if err := user.HashPassword(user.Password); err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	newUser := services.DB.Create(&user)
+	if newUser.Error != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": newUser.Error.Error()})
+		context.Abort()
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"userId": user.ID, "email": user.Email, "username": user.Username})
+
+}
