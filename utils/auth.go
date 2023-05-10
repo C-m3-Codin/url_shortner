72095ae0cd7e5bacf2926bed6a3a7df42628aa8a
c1337 <cyril199897@gmail.com>
2023-05-11 01:48:25 +0530

    auth re written

--- utils/auth.go
+++ utils/auth.go
@@ -2,54 +2,77 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
-// get the secret key from the env
-var jwtKeys = []byte("secretKEy")
+var jwtKey = []byte("my_secret_key")
 
-// Generates JWT token for a given username and email expiry set to 1 hr
-func GenerateJWT(email string, username string) (token string, err error) {
-	expirationTime := time.Now().Add(time.Hour * 1)
+// Claims represents the JWT claims.
+type Claims struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	jwt.StandardClaims
+}
 
-	claims := &jwt.MapClaims{
-		"email":    email,
-		"username": username,
-		"issuer":   "cp",
-		"exp":      expirationTime,
-		"data": map[string]string{
-			"name": "JohnDoe",
+// GenerateToken generates a new JWT token for the given username and email.
+func GenerateJWT(email string, username string) (string, error) {
+	// Set the expiration time of the token to 1 hour from now.
+	expirationTime := time.Now().Add(time.Hour)
+
+	// Create the JWT claims, which includes the username, email, and expiration time.
+	claims := &Claims{
+		Username: username,
+		Email:    email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "my_app",
+			Subject:   "auth",
 		},
 	}
 
-	token, err = jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(jwtKeys))
+	// Create the token using the HS256 algorithm and the JWT claims.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-	// token, err = token.SignedString(jwtKey)
-	return
+	// Sign the token with the secret key and return the signed token as a string.
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
 
-// checks the validity of a signed token returns nil if valid
-func ValidateToken(signedToken string) (err error) {
-	token, err := jwt.Parse(signedToken, func(token *jwt.Token) (interface{}, error) {
-		return []byte(jwtKeys), nil
+// ValidateToken validates the given JWT token and returns an error if the token is invalid.
+func ValidateToken(tokenString string) error {
+	// Parse the token using the HS256 algorithm and the secret key.
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			fmt.Println("\n\n error in sign method \n\n")
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtKey, nil
 	})
 
+	// Check if there was an error parsing the token.
 	if err != nil {
-		return
+		fmt.Println("\n\n error parsing \n\n")
+		if err == jwt.ErrSignatureInvalid {
+			fmt.Println("\n\n invalid token signature  \n\n")
+			return errors.New("invalid token signature")
+		}
+		return err
 	}
-	claims := token.Claims.(jwt.MapClaims)
 
-	exp, err := claims.GetExpirationTime()
-	if err != nil {
-		return
+	// Check if the token is valid.
+	if !token.Valid {
+		fmt.Println("\n\n token not valid \n\n")
+		return errors.New("invalid token")
 	}
 
-	if exp.Time.Local().Unix() < time.Now().Local().Unix() {
-		err = errors.New("token expired")
-		return
-	}
-	return
+	fmt.Println("\n\n all well \n\n")
+
+	return nil
 }
