c7f7565e7bb7e1124d9209846d92b1c9193a5a4b
c1337 <cyril199897@gmail.com>
2023-10-20 23:57:16 +0530

    all routes init

---
+++ handlers/allRoutes.go
@@ -0,0 +1,9 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetRoutes(c *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -62,6 +62,8 @@ func main() {
 		// Define a route for creating shortened URLs.
 		secured.POST("/create", handlers.CreateShortLink)
 
+		secured.GET("/allRoutes", handlers.GetHits)
+
 		secured.GET("/hits", handlers.GetHits)
 
 		// route to logout and invalidate the jwt
