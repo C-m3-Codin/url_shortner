9e478053560c988b8afa2d8ec4c160c801fdd0da
c1337 <cyril199897@gmail.com>
2023-10-25 23:57:49 +0530

    all hits for a particular shortenned url

--- handlers/allHits.go
+++ handlers/allHits.go
@@ -1,27 +1,44 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/c-m3-codin/url_shortner/models"
+	"github.com/c-m3-codin/url_shortner/services"
+	"github.com/gin-gonic/gin"
+)
 
 func GetHits(c *gin.Context) {
 
-	// userID := c.GetUint("userID") // Replace with the actual user's ID.
-	// fmt.Println("Hit all hits", userID)
+	userID := c.GetUint("userID") // Replace with the actual user's ID.
 
-	// // Query the user and preload their associated ShortLinks.
-	// var user models.User
+	shortUrl_received := c.Param("shortenedUrl")
+	fmt.Println("Hit all hits with userID: ", userID, " and shortURL: ", shortUrl_received)
 
-	// if result := services.DB.Preload("ShortLinks").First(&user, userID); result.Error != nil {
-	// 	if result.Error == gorm.ErrRecordNotFound {
-	// 		fmt.Println("User not found")
-	// 	} else {
-	// 		panic("Failed to load user details")
-	// 	}
-	// } else {
-	// 	fmt.Printf("User: %s\n", user.Name)
-	// 	fmt.Println("	:")
-	// 	for _, shortLink := range user.ShortLinks {
-	// 		fmt.Printf("- URL: %s\n", shortLink.OriginalURL)
-	// 	}
-	// }
+	if shortUrl_received == "" {
+		c.Header("Content-Type", "application/json")
+		c.JSON(http.StatusBadRequest, "Erron: Request with no shortURL ")
+		return
+	}
+
+	var shorturl models.ShortLink
+	services.DB.Where("shortened_url = ?", shortUrl_received).Find(&shorturl)
+
+	// check if authorised to check the given url
+	if shorturl.UserID != userID {
+		c.Header("Content-Type", "application/json")
+		c.JSON(http.StatusUnauthorized, "Erron: Not Authorised to access ")
+		return
+
+	}
+	type RedirectRequests_list []models.RedirectRequests
+	var allHits RedirectRequests_list
+	services.DB.Where("shortened_url = ?", shortUrl_received).Find(&allHits)
+	fmt.Println(allHits)
+
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, allHits)
+	return
 
 }
